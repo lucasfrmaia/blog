2b703ad382a94a6d11c926ae8d9940938a9a452c
yLexter <lucasmaia5954@gmail.com>
2024-05-04 20:34:32 -0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "/globals.css";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const fontSans = FontSans({
+   subsets: ["latin"],
+   variable: "--font-sans",
+});
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+   return (
+      <html lang="en">
+         <body
+            className={cn(
+               "bg-background font-sans antialiased",
+               fontSans.variable
+            )}
+         >
+            {children}
+         </body>
+      </html>
+   );
+}
