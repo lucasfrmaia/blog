a5f901b15c34fd76e7a997fefd23d770b28e3efc
yLexter <lucasmaia5954@gmail.com>
2024-07-26 02:54:53 -0300

    refactor: file of constants

---
+++ utils/constantes/routes.ts
@@ -0,0 +1,34 @@
+export const ROUTES_PAGE = {
+   home: {
+      link: "/",
+      label: "Início",
+   },
+   posts: {
+      link: "/posts",
+      label: "Posts",
+   },
+   contact: {
+      link: "/",
+      label: "Contato",
+   },
+   about: {
+      link: "/",
+      label: "Sobre",
+   },
+   register: {
+      link: "/register",
+      label: "Cadastra-se",
+   },
+   termsOfUse: {
+      link: "/terms-of-use",
+      label: "Termos de Uso",
+   },
+   login: {
+      link: "/login",
+      label: "Login",
+   },
+   recovery: {
+      link: "/recovery",
+      label: "Esqueci minha senha",
+   },
+};
