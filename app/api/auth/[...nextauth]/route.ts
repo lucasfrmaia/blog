7d4cb54de533f1bce90b7dcd79977d88bf668ed1
yLexter <lucasmaia5954@gmail.com>
2024-07-26 02:56:25 -0300

    feat: add next auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import { ROUTES_PAGE } from "@/utils/constantes/routes";
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const NextAuthOptions: AuthOptions = {
+   providers: [
+      CredentialsProvider({
+         id: "credentials",
+         name: "credentials",
+         credentials: {
+            username: {
+               type: "text",
+            },
+            password: {
+               type: "password",
+            },
+         },
+
+         async authorize(credentials, req) {
+            return null;
+         },
+      }),
+   ],
+   pages: {
+      signIn: ROUTES_PAGE.login.link,
+   },
+};
+
+export const handler = NextAuth(NextAuthOptions);
+
+export { handler as GET, handler as POST, NextAuthOptions };
